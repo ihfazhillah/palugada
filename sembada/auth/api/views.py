2023ba146f16bcf13e23d19c4b802edbbba0618e
ihfazhillah <mihfazhillah@gmail.com>
2021-07-04 17:07:45 +0700

    update salah nama

--- sembada/auth/api/views.py
+++ sembada/auth/api/views.py
@@ -1,12 +1,12 @@
 from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
 from rest_auth.registration.views import SocialLoginView
 from rest_auth.registration.serializers import SocialLoginSerializer
-from allauth.socialaccount.providers.oauth2.client import Oauth2Client
+from allauth.socialaccount.providers.oauth2.client import OAuth2Client
 
 # google view
 class GoogleLoginView(SocialLoginView):
     adapter_class = GoogleOAuth2Adapter
-    client_class = Oauth2Client
+    client_class = OAuth2Client
     serializer_class = SocialLoginSerializer
 
     def get_serializer(self, *args, **kwargs):
